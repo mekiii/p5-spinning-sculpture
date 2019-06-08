80a5c934157d16ed712053d887b30edd0c60ed06
Meki <you@example.com>
2019-06-08 14:36:56 +0200

    Playing around with angle speed

--- sketch.js
+++ sketch.js
@@ -9,12 +9,12 @@ function setup() {
   createCanvas(windowWidth, windowHeight, WEBGL);
   background(0);
   // put setup code here
-  number = 50;
+  number = 40;
 
   colorMode(RGB, 255, 255, 255, 1);
   stroke(24, 202, 230, 0);
   strokeWeight(1);
-  fill(0,0,0,0.2);
+  fill(0,0,0,0.05);
   blendMode(OVERLAY);
   radius = 1000;
   rad = 0;
@@ -29,7 +29,7 @@ function draw() {
   background(0, 0, 0, 0.9);
   counter += 0.01;
   for (let i = 0; i <= number; i++) {
-    radius = 5 //noise(counter) * 15;
+    radius = 6 //noise(counter) * 15;
     rad += 0.05;
     let speed = i/number 
     let angle = radians(rad);
@@ -38,17 +38,15 @@ function draw() {
    // rotateZ(PI/number);
     beginShape();
     //---------------start
-    curveVertex(0, -windowHeight / 2 + 20, 0);
-    curveVertex(0, -windowHeight / 2 + 20, 0);
+    vertex(0, -windowHeight / 2 + 50, 0);
     //---------------middle shit
-    curveVertex(-noise(counter) * radius * 2 * i * sin(angle * speed), -200 * noise(counter), radius * i * cos(angle * speed));
-    //vertex(-noise(counter) * radius * 1.7 * i * sin(angle * speed), -100 * noise(counter), radius * i * cos(angle * speed));
+    vertex(noise(counter) * radius * 2 * i * sin(angle + speed), -200 * noise(counter), radius * i * cos(angle + speed));
+    vertex(-noise(counter) * radius * i * sin(angle * speed), -100 * noise(counter), radius * i * cos(angle * speed));
     //vertex(-radius * i * sin(angle * speed), 0, radius * i * cos(angle * speed));
-    curveVertex(radius * i * cos(angle + speed), +200 * noise(counter), radius * i * sin(angle + speed));
-    curveVertex(-radius * i * sin(angle * speed), 100, radius * i * cos(angle * speed));
+    vertex(radius * i * cos(angle + speed),counter, radius * i * sin(angle + speed));
+    vertex(-radius * i * sin(angle + speed), 300, - radius * i * cos(angle + speed));
     //---------------end
-    curveVertex(0, windowHeight / 2 - 20, 0);
-    curveVertex(0, windowHeight / 2 - 20, 0);
+    vertex(0, windowHeight / 2 - 50, 0);
     endShape();
     //pop();
   }
